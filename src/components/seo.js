cf7370984812b9c548d4577ad3e7ce8fcfa64c86
Jaymes Downer <jaymesdowner@gmail.com>
2020-06-13 17:03:28 -0500

    twitter card

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import profile from '../../content/assets/profile-pic.jpg';
 
 function SEO({ description, lang, meta, keywords, title }) {
   const { site } = useStaticQuery(
@@ -66,6 +67,10 @@ function SEO({ description, lang, meta, keywords, title }) {
         {
           name: `twitter:description`,
           content: metaDescription,
+        },
+        {
+          name: `twitter:image`,
+          content: profile,
         },
       ]
         .concat(
