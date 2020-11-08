54bbca0d81d6b28b0f37fefde64b569cf5ab8a0b
Jaymes Downer <jaymesdowner@baymax.lan>
2020-11-08 16:24:20 -0600

    update job title \

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,12 +22,11 @@ class IndexPage extends React.Component {
         </h1>
 
         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <p>My name is <i><b>Jaymes</b></i>, and I lead the amazing Product Team at <a href="https://filmandmusic.com/" target="_blank" rel="noopener noreferrer">F&M</a>.</p>
+          <p>My name is <i><b>Jaymes</b></i>, and I'm currently the Head of Technology at <a href="https://fm.co/" target="_blank" rel="noopener noreferrer">FM</a>.</p>
         </div>
-
-
         
         <p>We serve the creative community by providing platforms for the best stock music and footage at <a href="https://musicbed.com" target="_blank" rel="noopener noreferrer">Musicbed</a> and <a href="https://filmsupply.com" target="_blank" rel="noopener noreferrer">Filmsupply</a>.</p>
+
         <p>Our Product Team is spread all around the world, and is full of some of the best people I’ve ever met.{" "}
           <span role="img" aria-label="raised hands emoji">
             🌎 ✈️ 👨🏼‍💻 👩🏾‍💻 🙌🏼
@@ -35,9 +34,9 @@ class IndexPage extends React.Component {
         </p>
         <p><i><b>I’m passionate about technology, but have grown even more passionate about leadership and helping team members see and reach their full potential.</b></i></p>
         <p>Here are some of my <Link to="/reading/">favorite books about leadership and growth</Link>.</p>
-        <p>I also <Link to="/blog/">ocassionally write</Link> about leadership, growth, and technology on my <Link to="/blog/">blog</Link>.</p>
+        <p>I <Link to="/blog/">ocassionally write</Link> about technology, growth, and leadership on my <Link to="/blog/">blog</Link>.</p>
 
-        <p>If you're curious what it's like to work for me, I created a <Link to="/manager-read-me">manager read me</Link>.</p>
+        <p>If you're curious what it's like to work with me, I created a <Link to="/manager-read-me">manager read me</Link> to aid in onboarding new team members.</p>
 
         <p><b>If there is anything I can do to help you, please let me know!</b></p>
 
